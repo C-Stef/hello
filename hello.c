f4d61d318fd71579f5d297dc08405493d5cd1d69
C-Stef <csteffel2@huskers.unl.edu>
2022-08-25 17:53:07 +0000

    added my minor

--- hello.c
+++ hello.c
@@ -8,6 +8,7 @@ int main(int argc, char **argv) {
 
     printf("Hello, I'm Camden Steffel!\n");
     printf("My major is Computer Engineer!\n");
+    printf("My minor is Sleeping in!\n");
 
     return 0;
 }
