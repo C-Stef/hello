fa035357c3abd31789334495a30833b744098302
C-Stef <csteffel2@huskers.unl.edu>
2022-08-25 17:37:26 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,11 @@
+/**
+ * Basic Hello World Program
+ */
+
+int main(int argc, char **argv) {
+
+    printf("Hello, I'm Camden Steffel!\n");
+    printf("My major is Computer Engineer!\n");
+
+    return 0;
+}
